ca0df5f02f2deb7f6289fb8e833663cd373a4a14
wanbowen <wbw157@gmail.com>
2022-08-23 19:30:21 +0800

    feat: update whitepaper url

--- src/config/const.js
+++ src/config/const.js
@@ -1,7 +1,7 @@
 export const linkList = {
   docs: "https://docs.linkkey.tech",
   blog: "https://mirror.xyz/linkkeyio.eth",
-  whitePaper: "https://linkkey.io/document/whitepaper",
+  whitePaper: "https://linkkey.io/document/whitepaper.pdf",
   sns: "https://sns.chat/",
   linkChat: "#",
   roadMap: "https://roadmap.linkkey.tech/",
