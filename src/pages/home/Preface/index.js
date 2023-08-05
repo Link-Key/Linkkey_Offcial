4f276e9d8c82e960f067f2096700979b271a3a47
Jassen <jassenlyou@foxmail.com>
2023-08-05 18:10:08 +0800

    feat: update

--- src/pages/home/Preface/index.js
+++ src/pages/home/Preface/index.js
@@ -1,4 +1,4 @@
-import { Box, Link, Typography } from '@mui/material';
+import { Box, Link, Stack, Typography } from '@mui/material';
 import { memo } from 'react';
 import NASA from '../../../assets/images/nasa-1.jpg';
 import { ReactComponent as NASALogo } from '../../../assets/icon/nasa_logo.svg';
@@ -35,11 +35,6 @@ const Preface = () => {
 						height: '60px',
 					}}
 				/>
-				<Link href="https://twitter.com/NASA_coins" target="_blank">
-					<TwitterIcon
-						sx={{ fontSize: '24px', color: '#1d9bf0', cursor: 'pointer' }}
-					/>
-				</Link>
 			</Box>
 			<Box
 				sx={{
@@ -49,21 +44,33 @@ const Preface = () => {
 					textAlign: 'center',
 				}}
 			>
-				<Typography
-					className="shine"
-					sx={{
-						fontSize: '68px',
-						fontWeight: 600,
-						// color: ' #fff',
-						// textShadow:
-						// 	'0 0 5px #fff,0 0 10px #fff,0 0 20px #fff,0 0 40px #0ff,0 0 80px #0ff,0 0 90px #0ff,0 0 100px #0ff,0 0 150px #0ff;',
-					}}
+				<Stack
+					direction="row"
+					justifyContent="center"
+					alignItems="center"
+					spacing={5}
 				>
-					NASA COIN
-				</Typography>
+					<Typography
+						className="shine"
+						sx={{
+							fontSize: '68px',
+							fontWeight: 600,
+							// color: ' #fff',
+							// textShadow:
+							// 	'0 0 5px #fff,0 0 10px #fff,0 0 20px #fff,0 0 40px #0ff,0 0 80px #0ff,0 0 90px #0ff,0 0 100px #0ff,0 0 150px #0ff;',
+						}}
+					>
+						Launch $LC
+					</Typography>
+					<Link href="https://twitter.com/Launch_XID" target="_blank">
+						<TwitterIcon
+							sx={{ fontSize: '48px', color: '#1d9bf0', cursor: 'pointer' }}
+						/>
+					</Link>
+				</Stack>
 
-				<Typography className="shine" sx={{ fontSize: '16px' }}>
-					Take everyone to the sky!
+				<Typography className="shine" sx={{ fontSize: '22px' }}>
+					We've gone live to continue our journey with our new brand Launch!
 				</Typography>
 			</Box>
 		</Box>
